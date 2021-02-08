d5acbf26039aefe7a9d9db24b8e602007f29fb3e
enceladus3 <ttavyy@gmail.com>
2021-02-08 19:52:13 +0200

    Second change

--- src/Customer.java
+++ src/Customer.java
@@ -18,58 +18,48 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
 
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while(rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            //Determine amounts for each line
-            thisAmount = amountFor(each);
-
-            //add freuent renter points
-            frequentRenterPoints++;
-
-            // Add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) frequentRenterPoints++;
+            frequentRenterPoints+=each.getFrequentRenterPoints();
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t";
-            result += String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-
-            //add footer lines
-            result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-            result += "You earned " + String.valueOf(frequentRenterPoints);
-            result += " frequent renter points";
-
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(each.getCharge()) + "\n";
         }
+        //add footer lines
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
+                + " frequent renter points";
+
         return result;
     }
 
     private double amountFor(Rental aRental) {
+        return aRental.getCharge();
+    }
+
+    private double getTotalCharge() {
         double result = 0;
-        switch(aRental.getMovie().getPriceCode()){
-            case Movie.REGULAR:
-                result += 2;
-                if(aRental.getDaysRented() > 2){
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if(aRental.getDaysRented() > 3){
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
